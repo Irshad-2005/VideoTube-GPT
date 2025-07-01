ef8d1799028234f938aa39dd31531cfece4b5134
Irshad Ali <irshadsheikh2005@gmail.com>
2025-07-02 01:09:42 +0530

    feat: add Appwrite integration for user authentication in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,15 @@
-import React, { useRef, useState } from 'react'
+import  { useRef, useState } from 'react'
 import Header from './Header'
 import { checkValidateData } from '../utils/validate';
+import { Client, Account, ID } from "appwrite";
 const Login = () => {
     const [isSignInForm , setIsSignInForm] = useState(true);
     const [errorMessage,setErrorMessage] = useState(null);
+
+    const client = new Client()
+    .setProject("67bdf9b10001d5d2c26b");
+
+    const account = new Account(client);
 
     const toggleToSignIn = ()=>
     {
@@ -12,6 +18,7 @@ const Login = () => {
 
     const email = useRef(null);
     const password = useRef(null);
+    const name = useRef(null);
 
     const handleSignInSignUpForm = ()=>
     {
@@ -19,12 +26,40 @@ const Login = () => {
         // console.log(email.current.value);
         // console.log(password.current.value);
         const message = checkValidateData(email.current.value,password.current.value);
-        setErrorMessage(message)
+        setErrorMessage(message);
+
+        if(message) return;
+
+        //sign in and sign up
+
+        if(!isSignInForm)
+        {
+            //sign up form
+        const user = account.create(ID.unique(),email.current.value,password.current.value,name.current.value);
+        user.then((resp)=>
+        {
+            console.log(resp);
+        }).catch((error)=>
+        {
+            console.log(error);
+        });
+        }
+        else{
+            //sign in form
+            const user = account.createEmailPasswordSession(email.current.value, password.current.value);
+            user.then((resp)=>
+            {
+                console.log("login : ", resp);
+            }).catch((error)=>
+            {
+                console.log( "error : ",error);
+            });
+        }
     }
     return (
         <div >
             <Header/>
-            <div className='w-full h-screen absolute'>
+            <div className='w-full h-screen absolute a'>
                 <img className="w-full h-screen" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f562aaf4-5dbb-4603-a32b-6ef6c2230136/dh0w8qv-9d8ee6b2-b41a-4681-ab9b-8a227560dc75.jpg/v1/fill/w_1192,h_670,q_70,strp/the_netflix_login_background__canada__2024___by_logofeveryt_dh0w8qv-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzIwIiwicGF0aCI6IlwvZlwvZjU2MmFhZjQtNWRiYi00NjAzLWEzMmItNmVmNmMyMjMwMTM2XC9kaDB3OHF2LTlkOGVlNmIyLWI0MWEtNDY4MS1hYjliLThhMjI3NTYwZGM3NS5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.LOYKSxIDqfPwWHR0SSJ-ugGQ6bECF0yO6Cmc0F26CQs"
                     alt="background"/>
             </div>
@@ -32,7 +67,12 @@ const Login = () => {
                 <form onSubmit={(e)=>{e.preventDefault()}}
                 className='absolute p-12 bg-black my-32 mx-auto right-0 left-0 w-3/12 opacity-85 text-white'>
                     <h1 className='font-bold text-3xl p-2 my-2 '> {isSignInForm ? "Sign In":"Sign Up"}</h1>
-                    {!isSignInForm && <input type="text" placeholder='Full Name' className='p-3 my-4 bg-gray-600 w-full rounded-sm'/>}
+                    {!isSignInForm && 
+                    <input 
+                    ref={name}
+                    type="text" 
+                    placeholder='Full Name' 
+                    className='p-3 my-4 bg-gray-600 w-full rounded-sm'/>}
                     <input 
                     ref={email}
                     type="email" 
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCQQ3Mdm39pYSmJXytTRsY2JFWhA4SxssU",
     authDomain: "videotube-gpt.firebaseapp.com",
     projectId: "videotube-gpt",
     storageBucket: "videotube-gpt.firebasestorage.app",
     messagingSenderId: "448152582190",
-    appId: "1:448152582190:web:0b3f234162c7c6f727c7bf",
-    measurementId: "G-DDH8047P2M"
+    appId: "1:448152582190:web:04adf7e2c0fd183127c7bf",
+    measurementId: "G-5EHC4FXCBF"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
